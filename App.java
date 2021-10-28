54b82ea996fe0982b61facb48b98313f882cde90
Myrian <myrianhf95@gmail.com>
2021-10-28 13:43:45 +0200

    Añadido nombre y fecha

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Myrian");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("28/10/2021");
   }
   
 }
